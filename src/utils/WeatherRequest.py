2dcde4cc36597dd5878d6485d49c19cf60ab2732
André Almeida <andrejunior573@gmail.com>
2024-02-25 20:17:47 -0300

    Adicionar Data de extração

--- src/utils/WeatherRequest.py
+++ src/utils/WeatherRequest.py
@@ -1,8 +1,11 @@
 import requests
 from config_api.config import link
+from datetime import datetime
+
 
 class WeatherRequest:
     """ Classe que ficará responsável pelas requisições das previsões do tempo """
+
     def __init__(self, link):
         self.link = link
 
@@ -13,6 +16,7 @@ class WeatherRequest:
             return request.json()
         return print("Erro ao fazer requisição", request.status_code)
 
+
 tempo = WeatherRequest(link)
 json_data = tempo.get_temperatura()
 local = json_data['name']
@@ -20,8 +24,6 @@ descricao = json_data['weather'][0]['description'].capitalize()
 temperatura = json_data['main']['temp']
 sensacao = json_data['main']['feels_like']
 umidade = json_data['main']['humidity']
+data = json_data['dt']
 
-
-
-
-print(f"Local: {local}\nDescrição: {descricao}\nTemperatura: {temperatura - 273.15:.2f}ºC\nSensação Térmica: {sensacao - 273.15:.2f}\nUmidade: {umidade}")
+print(f"Local: {local}\nData: {datetime.utcfromtimestamp(data).date()}\nDescrição: {descricao}\nTemperatura: {temperatura - 273.15:.2f}ºC\nSensação Térmica: {sensacao - 273.15:.2f}\nUmidade: {umidade}")

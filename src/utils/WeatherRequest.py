0d920c344a307770e79f436ddf376306da142065
André Almeida <andrejunior573@gmail.com>
2024-03-05 22:50:58 -0300

    Requisição de previsão para 15 dias

--- src/utils/WeatherRequest.py
+++ src/utils/WeatherRequest.py
@@ -1,29 +1,25 @@
 import requests
-from config_api.config import link
-from datetime import datetime
-
+import config
+import json
 
 class WeatherRequest:
-    """ Classe que ficará responsável pelas requisições das previsões do tempo """
-
+    """ Classe que ficará responsável pelas requisições das previsões do tempo no período de 15 dias"""
     def __init__(self, link):
         self.link = link
 
-    def get_temperatura(self):
-        """ Método para fazer solicitação da API sobre a temperatura em celsius"""
+    def get(self):
+        """ Método para fazer solicitação da API"""
         request = requests.get(self.link)
         if request.status_code == 200:
             return request.json()
         return print("Erro ao fazer requisição", request.status_code)
 
 
-tempo = WeatherRequest(link)
-json_data = tempo.get_temperatura()
-local = json_data['name']
-descricao = json_data['weather'][0]['description'].capitalize()
-temperatura = json_data['main']['temp']
-sensacao = json_data['main']['feels_like']
-umidade = json_data['main']['humidity']
-data = json_data['dt']
-# ainda esturando a api advisor api clima tempo, quero trazer previsões
-print(f"Local: {local}\nData: {datetime.utcfromtimestamp(data).date()}\nDescrição: {descricao}\nTemperatura: {temperatura - 273.15:.2f}ºC\nSensação Térmica: {sensacao - 273.15:.2f}\nUmidade: {umidade}")
+
+tempo = WeatherRequest(config.link)
+previsao = tempo.get()
+#print(previsao['data']['date'])
+print(previsao)
+print(previsao['data'][0]['date_br'], previsao['data'][0]['humidity'])
+#print(json.dumps(previsao, indent=4, ensure_ascii=False))
+

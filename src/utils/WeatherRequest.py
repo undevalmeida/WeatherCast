c8ee87138e808df58c0d9b1d82964dea4577ad5d
André Almeida <andrejunior573@gmail.com>
2024-03-08 23:51:00 -0300

    formatação do json

--- src/utils/WeatherRequest.py
+++ src/utils/WeatherRequest.py
@@ -7,27 +7,19 @@ class WeatherRequest:
     def __init__(self, link):
         self.link = link
 
-    def get(self):
+    @property
+    def three_days_forecast(self):
         """ Método para fazer solicitação da API"""
         request = requests.get(self.link)
         if request.status_code == 200:
-            return request.json()
+            previsao_tempo = request.json()
+
+            for data in previsao_tempo['data'][:3]:
+                print(f"Data: {data['date_br']}")
+                print(f"Probabilidade de Chuva: {data['rain']['probability']}%")
+                print(f"Temperatura: Minima - {data['temperature']['min']}Cº Máxima - {data['temperature']['max']}Cº")
+                print("-="*20)
         return print("Erro ao fazer requisição", request.status_code)
 
-
-
-
-tempo = WeatherRequest(link)
-previsao = tempo.get()
-
-def three_days_forecast():
-    """Retorna três próximos dias """
-
-
-temperatura = previsao['data'][1]['temperature']
-data = previsao['data'][1]
-
-print(f"Data: {previsao['data'][1]['date_br']}", f"\nProbabilidade de Chuva: {previsao['data'][1]['rain']['probability']}%", f"\nTemperatura: Minima - {temperatura['min']}Cº Máxima - {temperatura['max']}Cº")
-print("-" * 10)
-print(json.dumps(previsao, indent=4, ensure_ascii=False))
-
+weather_request = WeatherRequest(link)
+weather_request.three_days_forecast
